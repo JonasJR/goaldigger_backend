2408d3d81ba23ab9c6c3722f1d055391a6e21214
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2015-04-14 14:46:40 +0200

    Change to json respons

--- app/controllers/jsons_controller.rb
+++ app/controllers/jsons_controller.rb
@@ -1,7 +1,8 @@
 class JsonsController < ApplicationController
   def hello
     respond_to do |format|
-      format.json { render text: "Hello".to_json }
+      format.json { render text: "hello android"}
+      format.html { render }
     end
   end
 end
