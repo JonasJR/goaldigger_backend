02e7099049993567a81f4ab4d086194f9eff7a8e
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2015-04-28 15:28:41 +0200

    Refactor render_project to helper

--- app/controllers/jsons_controller.rb
+++ app/controllers/jsons_controller.rb
@@ -175,33 +175,7 @@ class JsonsController < ApplicationController
     end
   end
 
-  private 
-    
-    def render_projects
-      projects = @user.projects.all
-
-      projectList = []
-      projects.each do |project|
-        milestoneList = []
-
-        project.milestones.all.each do |milestone|
-          itemList = []
-
-          milestone.items.all.each do |item|
-            itemList << { id: item.id, name: item.name, done: item.done }
-          end
-
-          milestoneList << { id: milestone.id, name: milestone.name, items: itemList }
-
-        end
-        itemList = []
-        project.items.all.each do |item|
-          itemList << { id: item.id, name: item.name, done: item.done }
-        end
-        projectList << { id: project.id, name: project.name, description: project.description, items: itemList, milestones: milestoneList }
-      end
-      projectList
-    end
+  private
 
     def user_params
       params.permit(:name, :email, :password, :password_confirmation)
