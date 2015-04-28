ed8b23e5d657a3398932a934846ef7675f1e19f2
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2015-04-28 16:21:07 +0200

    Re-refactor render_project

--- app/controllers/jsons_controller.rb
+++ app/controllers/jsons_controller.rb
@@ -191,6 +191,32 @@ class JsonsController < ApplicationController
       end
     end
 
+    def render_projects
+    projects = @user.projects.all
+
+    projectList = []
+    projects.each do |project|
+      milestoneList = []
+
+      project.milestones.all.each do |milestone|
+        itemList = []
+
+        milestone.items.all.each do |item|
+          itemList << { id: item.id, name: item.name, done: item.done }
+        end
+
+        milestoneList << { id: milestone.id, name: milestone.name, items: itemList }
+
+      end
+      itemList = []
+      project.items.all.each do |item|
+        itemList << { id: item.id, name: item.name, done: item.done }
+      end
+      projectList << { id: project.id, name: project.name, description: project.description, items: itemList, milestones: milestoneList }
+    end
+    projectList
+  end
+
     def set_default_response_format
       request.format = :json
     end
