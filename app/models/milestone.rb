ea77f1d7d4fcb20c760952fb333d3e604507500d
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2015-05-05 15:17:36 +0200

    Add cascade to db

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,6 +1,6 @@
 class Milestone < ActiveRecord::Base
   belongs_to :project
-  has_many :items
+  has_many :items, dependent: :delete_all
 
   #validates :name, presence: true, length: { maximum: 30 }
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :user
-  has_many :milestones
+  has_many :milestones, dependent: :delete_all
 
   #validates :name, presence: true, length: { maximum: 30 }
 end
