76b026188647c9343b6d2e875fca0242fde1df5b
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2015-05-05 15:26:05 +0200

    Add dependent :destroy

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,6 +1,6 @@
 class Milestone < ActiveRecord::Base
   belongs_to :project
-  has_many :items, dependent: :delete_all
+  has_many :items, dependent: :destroy
 
   #validates :name, presence: true, length: { maximum: 30 }
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :user
-  has_many :milestones, dependent: :delete_all
+  has_many :milestones, dependent: :destroy
 
   #validates :name, presence: true, length: { maximum: 30 }
 end
