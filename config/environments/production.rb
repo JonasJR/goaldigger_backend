ff6aa20323374703c92ce13c5977bb94b2cdd45c
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2015-05-05 12:18:53 +0200

    Change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'goaldigger.herokuapp.com' }
-# ActionMailer Config
-# Setup for production - deliveries, no errors raised
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default :charset => "utf-8"
-config.action_mailer.smtp_settings = {
+  config.action_mailer.delivery_method = :smtp
+  # change to true to allow email to be sent during development
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
   domain: "goaldigger.com",
